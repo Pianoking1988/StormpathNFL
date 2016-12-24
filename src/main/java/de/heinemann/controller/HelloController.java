948ad4487b50181b143101ed659fec7c9279172f
Oliver Heinemann <oliver.schmitz.informatik+github@googlemail.com>
2016-12-24 10:18:29 +0100

    Add restricted path with account information output

--- src/main/java/de/heinemann/controller/HelloController.java
+++ src/main/java/de/heinemann/controller/HelloController.java
@@ -5,17 +5,29 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.stormpath.sdk.account.Account;
 import com.stormpath.sdk.application.Application;
+import com.stormpath.sdk.servlet.account.AccountResolver;
 import com.stormpath.sdk.servlet.application.ApplicationResolver;
 
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
-    public String hello(HttpServletRequest req) {
-        Application app = ApplicationResolver.INSTANCE.getApplication(req);
+    public String hello(HttpServletRequest request) {
+        Application application = ApplicationResolver.INSTANCE.getApplication(request);
         
-        return "Hello, " + app.getName();
+        return "Hello, " + application.getName();
+    }
+    
+    @RequestMapping("/restricted")
+    public String restricted(HttpServletRequest request) {
+    	Account account = AccountResolver.INSTANCE.getAccount(request);
+        if (account == null) {
+        	return "Access is not granted";
+        } else {
+        	return "Hello, your name is " + account.getFullName();
+        }
     }
     
 }
