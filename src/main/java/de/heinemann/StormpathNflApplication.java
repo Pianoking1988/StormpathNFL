768166fea561381b822e150cb15fa39544a61bb0
Oliver Heinemann <oliver.schmitz.informatik+github@googlemail.com>
2016-12-24 08:07:25 +0100

    Start project

---
+++ src/main/java/de/heinemann/StormpathNflApplication.java
@@ -0,0 +1,12 @@
+package de.heinemann;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StormpathNflApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StormpathNflApplication.class, args);
+	}
+}
