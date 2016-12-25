4c57cb2356606a93ed91d484bb8f046b96587547
Oliver Heinemann <oliver.schmitz.informatik+github@googlemail.com>
2016-12-25 15:15:24 +0100

    Within postgre, change tinyint to boolean type

--- src/main/resources/db/migration/postgre/V0.0.3__Create_match_table_postgre.sql
+++ src/main/resources/db/migration/postgre/V0.0.3__Create_match_table_postgre.sql
@@ -2,11 +2,11 @@ CREATE TABLE match (
     id         SERIAL,
     season     INTEGER     DEFAULT 2017 NOT NULL,
     week       INTEGER     DEFAULT 1 NOT NULL,
-    finished   TINYINT     DEFAULT 0 NOT NULL,
+    finished   BOOLEAN     DEFAULT 0 NOT NULL,
     homeTeam   INTEGER     DEFAULT 0 NOT NULL,
     roadTeam   INTEGER     DEFAULT 0 NOT NULL,
     homeScore  INTEGER     DEFAULT 0 NOT NULL,
     roadScore  INTEGER     DEFAULT 0 NOT NULL,
-    overtime   TINYINT     DEFAULT 0 NOT NULL,
+    overtime   BOOLEAN     DEFAULT 0 NOT NULL,
     PRIMARY KEY(id)
 );
