5e56e0c7c8385b3ea26da8694b22fc83f14269d6
Oliver Heinemann <oliver.schmitz.informatik+github@googlemail.com>
2016-12-25 15:20:06 +0100

    Change tinyint to boolean in hsqldb and refactor insert match script

--- src/main/resources/db/migration/common/V0.0.4__Fill match table week 8.sql
+++ src/main/resources/db/migration/common/V0.0.4__Fill match table week 8.sql
@@ -1,4 +1,4 @@
 INSERT INTO match (season, week, finished, homeTeam, roadTeam, homeScore, roadScore, overtime) VALUES
-(2017,  8, 1, 10, 12, 36, 22, 0),
-(2017,  8, 1,  7, 19, 27, 27, 1),
-(2017,  8, 0, 11, 14,  0,  0, 0);
+(2017,  8, TRUE , 10, 12, 36, 22, FALSE),
+(2017,  8, TRUE ,  7, 19, 27, 27, TRUE ),
+(2017,  8, FALSE, 11, 14,  0,  0, FALSE);
