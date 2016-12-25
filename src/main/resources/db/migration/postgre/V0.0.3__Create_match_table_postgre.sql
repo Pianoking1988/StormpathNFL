62c71a2141b030c67abb26d8cfe3961eb1440d2f
Oliver Heinemann <oliver.schmitz.informatik+github@googlemail.com>
2016-12-25 15:23:05 +0100

    Change default boolean value for postgre db

--- src/main/resources/db/migration/postgre/V0.0.3__Create_match_table_postgre.sql
+++ src/main/resources/db/migration/postgre/V0.0.3__Create_match_table_postgre.sql
@@ -1,12 +1,12 @@
 CREATE TABLE match (
     id         SERIAL,
-    season     INTEGER     DEFAULT 2017 NOT NULL,
-    week       INTEGER     DEFAULT 1 NOT NULL,
-    finished   BOOLEAN     DEFAULT 0 NOT NULL,
-    homeTeam   INTEGER     DEFAULT 0 NOT NULL,
-    roadTeam   INTEGER     DEFAULT 0 NOT NULL,
-    homeScore  INTEGER     DEFAULT 0 NOT NULL,
-    roadScore  INTEGER     DEFAULT 0 NOT NULL,
-    overtime   BOOLEAN     DEFAULT 0 NOT NULL,
+    season     INTEGER     DEFAULT 2017  NOT NULL,
+    week       INTEGER     DEFAULT 1     NOT NULL,
+    finished   BOOLEAN     DEFAULT FALSE NOT NULL,
+    homeTeam   INTEGER     DEFAULT 0     NOT NULL,
+    roadTeam   INTEGER     DEFAULT 0     NOT NULL,
+    homeScore  INTEGER     DEFAULT 0     NOT NULL,
+    roadScore  INTEGER     DEFAULT 0     NOT NULL,
+    overtime   BOOLEAN     DEFAULT FALSE NOT NULL,
     PRIMARY KEY(id)
 );
